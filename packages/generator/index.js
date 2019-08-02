2810480fe197e6bc3c5ea323a55214ca7baf9dfa
1846689910 <zuhe75@gmail.com>
2019-08-02 13:57:41 -0700

    back

--- packages/generator/index.js
+++ packages/generator/index.js
@@ -3,5 +3,27 @@ const Fs = require("fs");
 const Path = require("path");
 const rootDir = Path.dirname(Path.dirname(__dirname));
 
-xsh.rm("-rf", Path.join(rootDir, "tmp"));
+// needs to have a ready-to-use project then migrate to tmp
+// function generate(){
 
+// }
+
+// function demoAppStructure(){
+//   return {
+//     src: {
+//       client: {
+//         ".babelrc.js": ".babelrc.js"
+//       },
+//       server: {
+//         ".babelrc.js": ".babelrc.js"
+//       },
+//     },
+//     "package.json": "package.json",
+//     ".gitignore": ".gitignore"
+//   };
+// }
+
+// function initDir(){
+//   xsh.rm("-rf", Path.join(rootDir, "tmp"));
+//   xsh.mkdir("-p", Path.join(rootDir, "tmp/demo-app/client"), Path.join(rootDir, "tmp/demo-app/server"));
+// }

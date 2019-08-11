22296f7cf761c44efa9ac0be939dd6899c8889e6
1846689910 <eric14706@gmail.com>
2019-08-10 17:40:43 -0700

    mod

--- config/webpack/partial/html.js
+++ config/webpack/partial/html.js
@@ -1,6 +1,10 @@
 "use strict";
 
 const archetype = require("../../archetype");
+/**
+ *  reference: https://github.com/jantimon/html-webpack-plugin
+ *  根据template.html生成所需的index.html, 并且引用合适的bundle.js
+ * */
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = function() {

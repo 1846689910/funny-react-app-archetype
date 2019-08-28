bcc742792451f7cd6cf6c2178bdd4c989e572e28
1846689910 <eric14706@gmail.com>
2019-08-28 00:18:15 -0700

    add

---
+++ config/utils/ActionWatcher.js
@@ -0,0 +1,30 @@
+"use strict";
+
+class ActionWatcher {
+  constructor(onAction, afterAction, timeout) {
+    this._timer = null;
+    this._timeout = timeout;
+    this._onAction = typeof onAction === "function" ? onAction : null;
+    this._afterAction = typeof afterAction === "function" ? afterAction : null;
+    this._event = null;
+  }
+  watch = e => {
+    this._event = e;
+    if (e.persist) e.persist();
+    if (this._onAction) {
+      this._onAction(this._event);
+    }
+    if (this._timer) {
+      clearTimeout(this._timer);
+      this._timer = null;
+    }
+    this._timer = setTimeout(() => {
+      if (this._afterAction) {
+        this._afterAction(this._event);
+        this._event = null;
+      }
+    }, this._timeout);
+  };
+}
+
+module.exports = ActionWatcher;
